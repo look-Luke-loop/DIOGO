eaab1fef60c5a25c66474cdba93562fae509ed94
look_Luke_loop <lucasmiguel6000@gmail.com>
2023-12-07 23:19:10 -0300

    emoçoes

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./models/Users')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb://127.0.0.1:27017/crud")
+
+app.get('/',(req, res) =>{
+    UserModel.find({})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.get('/getUser/:id' , (req,res) => {
+    const id = req.params.id;
+    UserModel.findById({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+
+app.post("/createUser",(req, res) => {
+    UserModel.create(req.body)
+    .then(users => req.json(users))
+    .catch (err => res.json(err))
+})
+
+app.put("/updateUser/:id", (req,res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({_id:id},
+        {   
+            nome: req.body.nome, 
+            email: req.body.email,
+            idade: req.body.idade
+        })
+    .then(users => req.json(users))
+    .catch (err => res.json(err))
+})
+
+app.delete('/deleteUser/:id', (req,res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({_id:id})
+    .then(users => req.json(users))
+    .catch (err => res.json(err))
+
+})
+
+app.listen(3001, () => {
+    console.log("pai tá on")
+})
+
